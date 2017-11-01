8119549c04f7cf518fcc9bfa4d1a2171485285f2
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2017-11-01 12:12:25 +0100

    Display calculated values

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function getTriangleArea(a, h) {
   return (a * h) / 2
 }
 
-console.log(getTriangleArea(10, 6))
-
 var triangleAreaFirst = getTriangleArea(10, 15)
 var triangleAreaSecond = getTriangleArea(17, 12)
 var triangleAreaThird = getTriangleArea(-666, 15)
+
+console.log('getTriangleArea', getTriangleArea(10, 6))
+console.log('triangleAreaFirst', triangleAreaFirst)
+console.log('triangleAreaSecond', triangleAreaSecond)
+console.log('triangleAreaThird', triangleAreaThird)
