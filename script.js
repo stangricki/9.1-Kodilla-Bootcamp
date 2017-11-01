971c1980bfa76b10a289dbb9bd563e1e2a572020
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2017-11-01 12:11:17 +0100

    Introduce 3 variables

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getTriangleArea(a, h) {
-	if((a <= 0) || (h <= 0)) {
-		return("Nieprawidłowe dane")
-	}
-	return (a * h) / 2
+  if ((a <= 0) || (h <= 0)) {
+    return ("Nieprawidłowe dane")
+  }
+  return (a * h) / 2
 }
 
-console.log(getTriangleArea(10,6))
+console.log(getTriangleArea(10, 6))
 
-var triangle1Area = getTriangleArea(10, 15)
-var triangle1Area = getTriangleArea(17, 12)
-var triangle1Area = getTriangleArea(-666, 15)
+var triangleAreaFirst = getTriangleArea(10, 15)
+var triangleAreaSecond = getTriangleArea(17, 12)
+var triangleAreaThird = getTriangleArea(-666, 15)
