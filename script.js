bfc49f7535a63276f895b525003a2c46ca82d87e
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2017-11-01 12:15:22 +0100

    Use  for  calculated values

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var triangleAreaFirst = getTriangleArea(10, 15)
 var triangleAreaSecond = getTriangleArea(17, 12)
 var triangleAreaThird = getTriangleArea(-666, 15)
 
-console.log('getTriangleArea', getTriangleArea(10, 6))
-console.log('triangleAreaFirst', triangleAreaFirst)
-console.log('triangleAreaSecond', triangleAreaSecond)
-console.log('triangleAreaThird', triangleAreaThird)
+;[getTriangleArea(10, 6), triangleAreaFirst, triangleAreaSecond, triangleAreaThird].map(function(area) {
+  console.log('calculated area', area)
+})
